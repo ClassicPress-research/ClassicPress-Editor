085517fabf3b38d240c1140649d30d2629b9c1b2
John Alarcon <johnalarcon@users.noreply.github.com>
2021-09-07 20:06:51 -0700

    Fluff the code
    Add WP-style spacing to the PHP code.

--- classicpress-editor.php
+++ classicpress-editor.php
@@ -21,7 +21,7 @@
 
 namespace ClassicPress\TinyMce5;
 
-if (!defined('ABSPATH')) {
+if ( ! defined( 'ABSPATH' ) ) {
 	die();
 }
 
@@ -33,66 +33,78 @@ class Editor {
 		$tinymce_version = '591-20210827';
 
 		// Suppress Tiny in certain contexts.
-		if ($this->suppress_editor()) {
+		if ( $this->suppress_editor() ) {
 			return;
 		}
 
 		// Enqueue frontend assets.
-		add_action('wp_enqueue_scripts', [$this, 'enqueue_public_assets']);
+		add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_public_assets' ] );
 
 		// Ensure TinyMCE is loading from within this plugin, not core.
-		add_filter( 'includes_url', [$this, 'filter_tinymce_includes_url'], 10, 2 );
-		
+		add_filter( 'includes_url', [ $this, 'filter_tinymce_includes_url' ], 10, 2 );
+
 		// Filter the TinyMCE config object.
-		add_filter( 'tiny_mce_before_init', [$this, 'filter_tinymce_init'], 10, 2 );
-		
+		add_filter( 'tiny_mce_before_init', [ $this, 'filter_tinymce_init' ], 10, 2 );
+
 		// Filter the plugins loaded by TinyMCE.
-		add_filter( 'tiny_mce_plugins', [$this, 'filter_tinymce_plugins'], 11 );
-		
+		add_filter( 'tiny_mce_plugins', [ $this, 'filter_tinymce_plugins' ], 11 );
+
 		// Filter the buttons added to the TinyMCE editor.
-		add_filter( 'mce_buttons', [$this, 'filter_tinymce_buttons'], 10, 2 );
-		
+		add_filter( 'mce_buttons', [ $this, 'filter_tinymce_buttons' ], 10, 2 );
+
 		// Performant enqueuing for prism.js (for codesample TinyMCE plugin.)
-		add_filter('the_content', [$this, 'enqueue_prism_assets']);
+		add_filter( 'the_content', [ $this, 'enqueue_prism_assets' ] );
 
 	}
 
 	public function filter_tinymce_includes_url( $url, $path ) {
+
 		if ( strpos( $path, 'tinymce' ) !== false ) {
 			$url = plugins_url( $path, __FILE__ );
 		}
+
 		return $url;
+
 	}
 
 	public function filter_tinymce_init( $mceInit, $editor_id ) {
+
 		$mceInit['theme'] = 'silver'; //renaming silver folder to modern doesn't work
 		$mceInit['height'] = 700 + 75; //height now includes menu
 		$mceInit['min_height'] = 100 + 75;
 		$mceInit['resize'] = true; //old value 'vertical' not available
-
 		$mceInit['toolbar_location'] = 'top';
 		$mceInit['toolbar_persist'] = true;
 		$mceInit['custom_ui_selector'] = '.wp-editor-tools';
+
 		return $mceInit;
+
 	}
 
 	public function filter_tinymce_plugins( $plugins ) {
+
 		foreach ( array( 'wordpress', 'wplink', 'colorpicker', 'textcolor', 'wpautoresize' ) as $word ) {
 			if ( ($i = array_search( $word, $plugins )) !== false ) {
 				unset( $plugins[$i] );
 			}
 		}
-	//	$plugins[] = 'autoresize'; //while wpautoresize is not working
+
+		// $plugins[] = 'autoresize'; //while wpautoresize is not working
+
 		return $plugins;
+
 	}
 
 	public function filter_tinymce_buttons( $mce_buttons, $editor_id ) {
+
 		// This is how to add buttons.
 		//$mce_buttons[] = 'code';
 		//$mce_buttons[] = 'codesample';
+
 		return $mce_buttons;
+
 	}
-	
+
 	/**
 	 * Don't load Tiny for comments editor, quick draft editor, other
 	 */
@@ -130,17 +142,17 @@ class Editor {
 		 * on an as-needed basis in the enqueue_prism_assets() method which runs
 		 * on the the_content filter.
 		 */
-		wp_register_script('classicpress-editor-syntax-highlighter', plugin_dir_url(__FILE__).'js/prism.js', [], time(), true);
-		wp_register_style('classicpress-editor-syntax-highlighter',  plugin_dir_url(__FILE__).'styles/prism.css', [], time());
+		wp_register_script( 'classicpress-editor-syntax-highlighter', plugin_dir_url(__FILE__).'js/prism.js', [], time(), true );
+		wp_register_style( 'classicpress-editor-syntax-highlighter',  plugin_dir_url(__FILE__).'styles/prism.css', [], time() );
 
 	}
 
 	public function enqueue_prism_assets($content) {
 
 		// If <pre lang="whatever" is found in $content, enqueue prism assets.
-		if (preg_match('/<pre *(\w*? *= *["].*?["]|(\w*)*?)>/', $content) === 1) {
-			wp_enqueue_script('classicpress-editor-syntax-highlighter');
-			wp_enqueue_style('classicpress-editor-syntax-highlighter');
+		if ( preg_match( '/<pre *(\w*? *= *["].*?["]|(\w*)*?)>/', $content ) === 1 ) {
+			wp_enqueue_script( 'classicpress-editor-syntax-highlighter' );
+			wp_enqueue_style( 'classicpress-editor-syntax-highlighter' );
 		}
 
 		// Return the possibly-amended content.
@@ -152,4 +164,3 @@ class Editor {
 
 // Make beautiful all the things.
 new Editor;
-
