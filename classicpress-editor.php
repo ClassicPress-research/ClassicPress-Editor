e91f6ad3507233aad2b24afeea7e21438a75561d
Joy Reynolds <dev@joyreynolds.com>
2021-09-23 00:07:28 -0500

    Rework filters and config to match plugin changes. Add searchreplace plugin.

--- classicpress-editor.php
+++ classicpress-editor.php
@@ -4,7 +4,7 @@
  * -----------------------------------------------------------------------------
  * Plugin Name: ClassicPress Editor - Experimental
  * Description: An integration of TinyMCE version 5.9.  This plugin is not yet intended for production use.
- * Version: 1.0.1-alpha
+ * Version: 1.0.2-alpha
  * Author: John Alarcon, Joy Reynolds, and ClassicPress Contributors
  * Author URI: https://www.classicpress.net
  * Text Domain: classicpress-editor
@@ -29,19 +29,20 @@ class Editor {
 	public function __construct() {
 
 		global $tinymce_version;
-
 		$tinymce_version = '591-20210827';
 
-		// Filter TinyMCE plugins.
+		// Ensure scripts are loading from within this plugin, not core.
+		add_filter( 'includes_url', [ $this, 'filter_tinymce_includes_url' ], 10, 2 );
+		add_filter( 'script_loader_src', [ $this, 'filter_editor_script_url' ], 10, 2 );
+
+		// Visual Mode: filter the TinyMCE config object.
+		add_filter( 'tiny_mce_before_init', [ $this, 'filter_tinymce_init' ], 10, 2 );
+
+		// Visual Mode: filter core plugins.
 		add_filter( 'tiny_mce_plugins', [ $this, 'filter_tinymce_plugins' ], 11 );
 
-		// Ensure TinyMCE is loading from within this plugin, not core.
-		add_filter( 'includes_url', [ $this, 'filter_tinymce_includes_url' ], 10, 2 );
-		add_filter( 'script_loader_src', [ $this, 'filter_editor_expand_url' ], 10, 2 );
-
-		// Filter the TinyMCE config objects.
-		add_filter( 'tiny_mce_before_init', [ $this, 'filter_tinymce_init' ], 10, 2 );
-		add_filter( 'teeny_mce_before_init', [ $this, 'filter_teenymce_init' ], 10, 2 );
+		// Visual Mode: filter second row of buttons.
+		add_filter( 'mce_buttons_2', [ $this, 'filter_mce_buttons_2' ] );
 
 	}
 
@@ -52,51 +53,53 @@ class Editor {
 		}
 
 		return $url;
-		
+
 	}
 	
-	public function filter_editor_expand_url( $src, $handle ) {
+	public function filter_editor_script_url( $src, $handle ) {
 		if ( $handle === 'editor-expand' ) {
 			$src = plugins_url( 'js/editor-expand.js', __FILE__ );
+		}
+		if ( $handle === 'post' ) {
+			$src = plugins_url( 'js/post.js', __FILE__ );
+		}
+		if ( $handle === 'editor' ) {
+			$src = plugins_url( 'js/editor.js', __FILE__ );
 		}
 		return $src;
 	}
 
-
 	public function filter_tinymce_init( $mceInit, $editor_id ) {
 
 		$mceInit['theme'] = 'silver'; //renaming silver folder to modern doesn't work
-		$mceInit['height'] = 700 + 75; //height now includes menu
-		$mceInit['min_height'] = 100 + 75;
-		$mceInit['resize'] = true; //old value 'vertical' not available
-		$mceInit['toolbar_location'] = 'top';
-		$mceInit['toolbar_persist'] = true;
+//		$mceInit['height'] = 700 + 75; //height now includes menu
+//		$mceInit['min_height'] = 100 + 75;		
+//		$mceInit['resize'] = true; //old value 'vertical' 
+		
 		$mceInit['custom_ui_selector'] = '.wp-editor-tools';
 
 		return $mceInit;
-		
-	}
-
-	public function filter_teenymce_init( $mceInit, $editor_id ) {
-
-		return $mceInit;
 
 	}
-	
+
 	public function filter_tinymce_plugins( $plugins ) {
 
-		foreach ( array( 'wordpress', 'wplink', 'colorpicker', 'textcolor', 'wpautoresize' ) as $word ) {
+		foreach ( array( 'wplink', 'colorpicker', 'textcolor' ) as $word ) {
 			if ( ($i = array_search( $word, $plugins )) !== false ) {
 				unset( $plugins[$i] );
 			}
 		}
-
-		// $plugins[] = 'autoresize'; //while wpautoresize is not working
-
+		$plugins[] = 'searchreplace'; //add new feature, needs translations handled
+		$plugins[] = 'link'; //while wplink is not working
 		return $plugins;
-		
+
 	}
 
+	public function filter_mce_buttons_2( $buttons ) {
+		$hold = array_pop( $buttons );
+		array_push( $buttons, 'searchreplace', $hold );
+		return $buttons;
+	}
 
 }
 
