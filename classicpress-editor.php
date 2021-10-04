399d9387584e451e15fada902684d648e2fa5602
Joy <dev@joyreynolds.com>
2021-10-04 16:20:31 -0500

    add filters for teeny, cleanup plugin code and headers

--- classicpress-editor.php
+++ classicpress-editor.php
@@ -3,10 +3,8 @@
  * -----------------------------------------------------------------------------
  * Plugin Name: ClassicPress Editor update - Experimental
  * Description: Update to TinyMCE version 5.9.  This plugin is not yet intended for production use.
- * Version: 1.0.4-alpha
+ * Version: 1.0.5-alpha
  * Author: John Alarcon, Joy Reynolds, and ClassicPress Contributors
- * Author URI: https://www.classicpress.net
- * Text Domain: classicpress-editor
  * -----------------------------------------------------------------------------
  * This is free software released under the terms of the General Public License,
  * version 2, or later. It is distributed WITHOUT ANY WARRANTY; without even the
@@ -17,85 +15,89 @@
  * -----------------------------------------------------------------------------
  */
 
-namespace ClassicPress\TinyMce5;
-
 if ( ! defined( 'ABSPATH' ) ) {
 	die();
 }
 
-class Editor {
+global $tinymce_version;
+$tinymce_version = '591-20210827';
 
-	public function __construct() {
-		global $tinymce_version;
-		$tinymce_version = '591-20210827';
-
-		// Ensure scripts are loading from within this plugin, not core.
-		add_filter( 'includes_url', [ $this, 'filter_tinymce_includes_url' ], 10, 2 );
-		add_filter( 'script_loader_src', [ $this, 'filter_editor_script_url' ], 10, 2 );
-
-		// Visual Mode: filter the TinyMCE config object.
-		add_filter( 'tiny_mce_before_init', [ $this, 'filter_tinymce_init' ], 10, 2 );
-
-		// Visual Mode: filter core plugins.
-		add_filter( 'tiny_mce_plugins', [ $this, 'filter_tinymce_plugins' ], 11 );
-
-		// Visual Mode: filter second row of buttons.
-		add_filter( 'mce_buttons_2', [ $this, 'filter_mce_buttons_2' ] );
+// Ensure scripts are loading from within this plugin, not core.
+add_filter( 'includes_url', 'try_tinymce5_tinymce_includes_url', 10, 2 );
+add_filter( 'script_loader_src', 'try_tinymce5_editor_script_url', 10, 2 );
+function try_tinymce5_tinymce_includes_url( $url, $path ) {
+	if ( strpos( $path, 'tinymce' ) !== false ) {
+		$url = plugins_url( $path, __FILE__ );
 	}
-
-	public function filter_tinymce_includes_url( $url, $path ) {
-		if ( strpos( $path, 'tinymce' ) !== false ) {
-			$url = plugins_url( $path, __FILE__ );
-		}
-		return $url;
-	}
-	
-	public function filter_editor_script_url( $src, $handle ) {
-		if ( $handle === 'editor-expand' ) {
-			$src = plugins_url( 'js/editor-expand.js', __FILE__ );
-		}
-		if ( $handle === 'post' ) {
-			$src = plugins_url( 'js/post.js', __FILE__ );
-		}
-		if ( $handle === 'editor' ) {
-			$src = plugins_url( 'js/editor.js', __FILE__ );
-		}
-		return $src;
-	}
-
-	public function filter_tinymce_init( $mceInit, $editor_id ) {
-		$mceInit['theme'] = 'silver'; //renaming silver folder to modern doesn't work
-//		$mceInit['height'] = 300 + 75; //height now includes UI
-//		$mceInit['min_height'] = 100 + 75;
-//		$mceInit['resize'] = true; //old value 'vertical'
-		
-//		$mceInit['skin'] = 'darkgray';  //default is lightgray
-		$mceInit['toolbar_mode'] = 'sliding';  //still testing best option
-		$mceInit['toolbar_location'] = 'top'; //auto was added and set as the default in TinyMCE 5.3
-
-		// TinyMCE 5.5+ This option disables the automatic show and hide behavior of the toolbar and menu bar for inline editors
-		$mceInit['toolbar_persist'] = true;  //inline editor - needed for plugins?
-		$mceInit['custom_ui_selector'] = '.wp-editor-tools';
-		return $mceInit;
-	}
-
-	public function filter_tinymce_plugins( $plugins ) {
-		foreach ( array( 'wplink', 'colorpicker', 'textcolor' ) as $word ) {
-			if ( ($i = array_search( $word, $plugins )) !== false ) {
-				unset( $plugins[$i] );
-			}
-		}
-		$plugins[] = 'searchreplace'; //add new feature, needs translations handled
-		$plugins[] = 'link'; //while wplink is not working
-		return $plugins;
-	}
-
-	public function filter_mce_buttons_2( $buttons ) {
-		$hold = array_pop( $buttons );
-		array_push( $buttons, 'searchreplace', $hold );
-		return $buttons;
-	}
-
+	return $url;
 }
 
-new Editor;
+function try_tinymce5_editor_script_url( $src, $handle ) {
+	if ( $handle === 'editor-expand' ) {
+		$src = plugins_url( 'js/editor-expand.js', __FILE__ );
+	}
+	if ( $handle === 'post' ) {
+		$src = plugins_url( 'js/post.js', __FILE__ );
+	}
+	if ( $handle === 'editor' ) {
+		$src = plugins_url( 'js/editor.js', __FILE__ );
+	}
+	return $src;
+}
+
+// Visual Mode: filter the TinyMCE config object.
+add_filter( 'tiny_mce_before_init', 'try_tinymce5_tinymce_init', 11, 2 );
+function try_tinymce5_tinymce_init( $mceInit, $editor_id ) {
+	$mceInit['theme'] = 'silver'; //renaming silver folder to modern doesn't work
+//	$mceInit['height'] = 300 + 75; //height now includes UI
+//	$mceInit['min_height'] = 100 + 75;
+//	$mceInit['resize'] = true; //old value 'vertical' for boolean option
+
+//	$mceInit['skin'] = 'darkgray';  //core loads lightgray
+	$mceInit['toolbar_mode'] = 'sliding';  //still testing best option
+	$mceInit['toolbar_location'] = 'top'; //auto was added and set as the default in TinyMCE 5.3
+
+	$mceInit['custom_ui_selector'] = '.wp-editor-tools';
+	return $mceInit;
+}
+
+// Visual Mode: filter core plugins.
+add_filter( 'tiny_mce_plugins', 'try_tinymce5_tinymce_plugins', 11 );
+function try_tinymce5_tinymce_plugins( $plugins ) {
+	// colorpicker and textcolor were made part of 5.x core
+	foreach ( array( 'wplink', 'colorpicker', 'textcolor' ) as $word ) {
+		if ( ($i = array_search( $word, $plugins )) !== false ) {
+			unset( $plugins[$i] );
+		}
+	}
+	$plugins[] = 'searchreplace'; //add new feature, TODO: translations handled?
+	$plugins[] = 'link'; //while wplink is not working
+	return $plugins;
+}
+
+// Visual Mode: filter second row of buttons.
+add_filter( 'mce_buttons_2', 'try_tinymce5_mce_buttons_2' );
+function try_tinymce5_mce_buttons_2( $buttons ) {
+	$hold = array_pop( $buttons );
+	array_push( $buttons, 'searchreplace', $hold );
+	return $buttons;
+}
+
+// Text Mode: filter the config object.
+add_filter( 'teeny_mce_before_init', 'try_tinymce5_teenymce_init', 11, 2 );
+function try_tinymce5_teenymce_init( $mceInit, $editor_id ) {
+	// TinyMCE 5.5+ This option disables the automatic show and hide behavior of the toolbar and menu bar for inline editors
+	$mceInit['toolbar_persist'] = true;  //inline editor - needed for plugins?
+}
+
+// Text Mode: filter core plugins.
+add_filter( 'teeny_mce_plugins', 'try_tinymce5_teenymce_plugins', 11 );
+function try_tinymce5_teenymce_plugins( $plugins ) {
+	// colorpicker and textcolor were made part of 5.x core
+	foreach ( array( 'colorpicker', 'textcolor' ) as $word ) {
+		if ( ($i = array_search( $word, $plugins )) !== false ) {
+			unset( $plugins[$i] );
+		}
+	}
+	return $plugins;
+}
